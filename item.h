be7029904dd4bf23e7ba982633ea4ac74d444a2a
Imoen42 <72242659+Imoen42@users.noreply.github.com>
2021-09-25 08:30:23 +0800

    Update item.h

--- item.h
+++ item.h
@@ -1,20 +1,13 @@
 #ifndef _item_h
 #define _item_h
 
-struct Items
-{
-    char zh_name[10];
-    char name[12];
-    char description[500];
-	int player_score;
-    int turn_on;
-	char turn_on_description[50];
-	char turn_off_description[50];
-	char fail[60];
-};
-
-extern struct Items candle;
-extern struct Items violin;
-extern struct Items erhu;
+void init_item();
+void free_item();
+int search_item(char *args1);
+char *get_item_string(char *update, char *args1);
+void look_item(char *args1);
+void use_item(int player_score, char *args1);
+void set_item(char *args1);
+int item_on(char *args1);
 
 #endif
